8de61f7df3a390521cf57f1e51e06d83dc5eaf46
gicheol <57222722+jbs3047@users.noreply.github.com>
2024-02-14 12:06:14 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,31 +1,28 @@
+#10828 스택 - 시간초과
 import sys
-
 N = int(input())
-stack = []
-
-for _ in range(N):
-	prompt = sys.stdin.readline().split()
-	
-	if prompt[0] == 'push':
-		stack.append(prompt[1])
-	
-	elif prompt[0] == 'pop':
-		if stack:
-			print(stack.pop())
-		else:
-			print(-1)
-	
-	elif prompt[0] == 'size':
-		print(len(stack))
-
-	elif prompt[0] == 'empty':
-		if stack:
-			print(0)
-		else:
-			print(1)
-	
-	elif prompt[0] == 'top':
-		if stack:
-			print(stack[-1])
-		else:
-			print(-1)
+b=[]
+for i in range(N):
+    #a=list(input().split())
+    a= list(sys.stdin.readline().rstrip().split())
+    if a[0] == 'push': #얘 뺴고 다 출력
+        b.append(a[1])
+    elif a[0] == 'pop':
+        if len(b) == 0:
+            print(-1)
+        else:
+            print(b[-1])
+            del b[-1]
+    elif a[0] == 'size':
+        print(len(b))
+    elif a[0] == 'empty':
+        if len(b)==0:
+            print(1)
+        else:
+            print(0)
+    elif a[0] == 'top':
+        if len(b)==0:
+            print(-1)
+        else:
+            print(b[-1])
+    
