6863f5fddf9ba59e8a8830fcf257e9c36ee8e514
gicheol <57222722+jbs3047@users.noreply.github.com>
2024-02-14 15:16:21 +0900

    [Silver I] Title: 삼각 그래프, Time: 4236 ms, Memory: 72620 KB -BaekjoonHub

--- 백준/Silver/4883. 삼각 그래프/삼각 그래프.py
+++ 백준/Silver/4883. 삼각 그래프/삼각 그래프.py
@@ -7,14 +7,10 @@ while True:
     dp=[[0]*3 for _ in range(n)]
     dp[0]=[10000,lst[0][1],lst[0][1]+lst[0][2]]
     for i in range(1,n):
-        for j in range(3):
-            if j==0:
-                dp[i][0]=min(dp[i-1][0],dp[i-1][1])+lst[i][0]
-            elif j==1:
-                dp[i][1]=min(dp[i][0],dp[i-1][0],dp[i-1][1],dp[i-1][2])+lst[i][1]
-            else:
-                dp[i][2]=min(dp[i][1],dp[i-1][1],dp[i-1][2])+lst[i][2]
+        dp[i][0]=min(dp[i-1][0],dp[i-1][1])+lst[i][0]
+        dp[i][1]=min(dp[i][0],dp[i-1][0],dp[i-1][1],dp[i-1][2])+lst[i][1]
+        dp[i][2]=min(dp[i][1],dp[i-1][1],dp[i-1][2])+lst[i][2]
 
-    print(f'{cnt}. {dp[n-1][1]}')
+    print(f'{cnt}. {dp[n-1][1]}') 
     cnt+=1
 
