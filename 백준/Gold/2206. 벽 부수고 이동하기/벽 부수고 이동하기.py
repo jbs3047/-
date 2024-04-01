357a07d43375aa9ba9dc535fab804a2c4fddfcdb
gicheol <57222722+jbs3047@users.noreply.github.com>
2024-04-01 16:37:23 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 2572 ms, Memory: 78428 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -1,58 +1,27 @@
 from collections import deque
-dx=[-1,1,0,0]
-dy=[0,0,-1,1]
-n,m=map(int,input().split())#세로 가로
-lst=[list(map(int,input())) for _ in range(n)]
-#시작점 끝점 bfs 돌린 후 모든 벽에서 탐색 돌려서 좌우에 있으면 최소합
-lst2=[i[:] for i in lst]#시작점
-lst3=[i[:] for i in lst]#끝점
-def bfs(x,y,map):
+def bfs(x,y):
     q=deque()
-    q.append((x,y))
-    map[y][x]=2
+    q.append((0,y,x,1))
     while q:
-        x,y=q.popleft()
+        boom,y,x,cnt=q.popleft()
+        if x==m-1 and y==n-1:
+            return cnt
         for i in range(4):
             nx=x+dx[i]
             ny=y+dy[i]
             if 0<=nx<m and 0<=ny<n:
-                if map[ny][nx]==0:
-                    map[ny][nx]=map[y][x]+1
-                    q.append((nx,ny))
-def check(x,y):
-    chk1=n*m+2
-    chk2=n*m+2
-    for i in range(4):
-        nx=x+dx[i]
-        ny=y+dy[i]
-        if 0<=nx<m and 0<=ny<n and lst2[ny][nx]!=0 and lst2[ny][nx]!=1:
-            chk1=min(chk1,lst2[ny][nx])
-    for i in range(4):
-        nx=x+dx[i]
-        ny=y+dy[i]
-        if 0<=nx<m and 0<=ny<n and lst3[ny][nx]!=0 and lst3[ny][nx]!=1:
-            chk2=min(chk2,lst3[ny][nx])
-    #print(x,y)
-    #print(chk1,chk2)
-    #print('-----')
-    if chk1!=n*m+2 and chk2!=n*m+2:
-        return chk1+chk2-1
-    return n*m+2
+                if lst[ny][nx]==1 and boom<1 and not vis[boom+1][ny][nx]:# 일단 깰 수 있으면 무조건 깨고 넘어감 (미방문시)
+                    vis[boom+1][ny][nx]=1
+                    q.append((boom+1,ny,nx,cnt+1))
+                elif lst[ny][nx]==0 and not vis[boom][ny][nx]: #방문한 적 없음, 길 -> 일단 감
+                    vis[boom][ny][nx]=1
+                    q.append((boom,ny,nx,cnt+1))
+    return -1
 
-bfs(0,0,lst2)
-#print(lst2)
-bfs(m-1,n-1,lst3)
-#print(lst3)
-if lst3[0][0]!=0:
-    ans=lst3[0][0]-1
-else:
-    ans=n*m+2
-for i in range(n):
-    for j in range(m):
-        if lst[i][j]==1:
-            ans=min(ans,check(j,i))
-if ans==n*m+2:
-    print(-1)
-else:
-    print(ans)
-
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
+n,m=map(int,input().split())
+lst=[list(map(int,input())) for _ in range(n)]
+vis=[[[0]*(m) for _ in range(n)] for _ in range(2)]
+ans=bfs(0,0)
+print(ans)
